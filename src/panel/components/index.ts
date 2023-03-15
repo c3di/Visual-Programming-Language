a367ded45f3048323620248cc4ddb35311cb1db5
Fei Chen <boxchenfei@gmail.com>
2023-03-15 23:08:13 +0100

    Create a control panel component (#18)

--- src/panel/components/index.ts
+++ src/panel/components/index.ts
@@ -1,5 +1,6 @@
 import nodeTypes from './nodes';
 export { default as Background } from './Background';
+export { default as ControlPanel } from './ControlPanel';
 
 const componentType = {
   nodeTypes,
