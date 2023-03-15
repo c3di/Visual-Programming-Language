4f534cfa3f58d4c3f1e73e9edfaa1c240388c0b6
Fei Chen <boxchenfei@gmail.com>
2023-03-15 19:24:40 +0100

    Install the react-flow package and test the example (#5)

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+/* eslint-disable */
+
+import React, { useCallback } from 'react';
+import ReactFlow, {
+  addEdge,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  Edge,
+  Connection,
+} from 'reactflow';
+
+import {
+  nodes as initialNodes,
+  edges as initialEdges,
+} from './initial-elements';
+import CustomNode from './CustomNode';
+
+import 'reactflow/dist/style.css';
+import './overview.css';
+
+const nodeTypes = {
+  custom: CustomNode,
+};
+
+const minimapStyle = {
+  height: 120,
+};
+
+const OverviewFlow = () => {
+  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback((params) => {
+    setEdges((eds) => addEdge(params, eds));
+  }, []);
+
+  // we are using a bit of a shortcut here to adjust the edge type
+  // this could also be done with a custom edge for example
+  const edgesWithUpdatedTypes = edges.map((edge) => {
+    if (edge.sourceHandle) {
+      const edgeType = nodes.find((node) => node.type === 'custom')?.data
+        .selects?.[edge.sourceHandle];
+      // if (edgeType !== undefined) {
+      //   edge.type = edgeType[edge.sourceHandle];
+      // }
+      edge.type = edgeType;
+    }
+
+    return edge;
+  });
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edgesWithUpdatedTypes}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      fitView
+      attributionPosition="top-right"
+      nodeTypes={nodeTypes}
+    >
+      <MiniMap style={minimapStyle} zoomable pannable />
+      <Controls />
+      <Background color="#aaa" gap={16} />
+    </ReactFlow>
+  );
+};
+
+export default OverviewFlow;
