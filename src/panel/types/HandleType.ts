b08784f7a7e4344601daee5b302cd1c335b67f30
Fei Chen <boxchenfei@gmail.com>
2023-03-16 20:37:04 +0100

    Make some props optional in Handle (#37)

--- src/panel/types/HandleType.ts
+++ src/panel/types/HandleType.ts
@@ -4,8 +4,8 @@ export interface HandleType {
   type: 'source' | 'target';
   title: string; // keep unique for each node
   connected: boolean;
-  tooltip: string;
-  dataType: DataType;
-  defaultValue: any;
-  value: any;
+  tooltip?: string;
+  dataType?: DataType;
+  defaultValue?: any;
+  value?: any;
 }
