68323b46b8ea8b45b261891022f5abd76edd3648
Fei Chen <boxchenfei@gmail.com>
2023-03-16 23:27:58 +0100

    Create constant node type (#42)

--- src/panel/types/index.ts
+++ src/panel/types/index.ts
@@ -2,5 +2,6 @@ export { DataType, isDataTypeMatch } from './DataType';
 export type { default as Node } from './BasicNode';
 export { type Handle as HandleType } from './Handle';
 export { type FunctionNode as NodeType } from './FunctionNode';
+export { type ConstantNode } from './ConstantNode';
 export { type Edge as EdgeType } from './Edge';
 export { type Graph } from './Graph';
