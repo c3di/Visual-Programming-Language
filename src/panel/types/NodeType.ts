2b2d18de3feab25991a8d377b244f33416aa4103
Fei Chen <boxchenfei@gmail.com>
2023-03-16 19:58:53 +0100

    Append the default node type

--- src/panel/types/NodeType.ts
+++ src/panel/types/NodeType.ts
@@ -1,8 +1,11 @@
 import { type HandleType } from './HandleType';
+import { type Node as RfNode } from 'reactflow';
 
-export interface NodeType {
+interface NodeData {
   title: string;
   inputs: HandleType[];
   outputs: HandleType[];
   tooltip?: 'this is a customed node';
 }
+
+export type NodeType = RfNode<NodeData>;
