e11667113092c66d2d5948db673be6174eb35cdf
Fei Chen <boxchenfei@gmail.com>
2023-03-16 23:29:24 +0100

    Remove rename for the type

--- src/panel/types/index.ts
+++ src/panel/types/index.ts
@@ -1,8 +1,8 @@
 export { DataType, isDataTypeMatch } from './DataType';
 export type { default as Node } from './BasicNode';
-export { type Handle as HandleType } from './Handle';
-export { type FunctionNode as NodeType } from './FunctionNode';
+export { type Handle } from './Handle';
+export { type FunctionNode } from './FunctionNode';
 export { type ConstantNode } from './ConstantNode';
 export { type VariableNode } from './VariableNode';
-export { type Edge as EdgeType } from './Edge';
+export { type Edge } from './Edge';
 export { type Graph } from './Graph';
