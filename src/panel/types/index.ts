124a47b85082ed6b7256a47175cab7201e4c6329
Fei Chen <boxchenfei@gmail.com>
2023-03-16 20:40:50 +0100

    Add edge type with data type property (#38)

--- src/panel/types/index.ts
+++ src/panel/types/index.ts
@@ -1,3 +1,4 @@
 export { DataType, isDataTypeMatch } from './DataType';
 export { type HandleType } from './HandleType';
 export { type NodeType } from './NodeType';
+export { type EdgeType } from './EdgeType';
