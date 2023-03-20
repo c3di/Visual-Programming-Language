9536b1181a4b4c22932cc791785abe794f89b7af
Fei Chen <boxchenfei@gmail.com>
2023-03-20 16:17:56 +0100

    Rename

--- src/panel/types/Variable.ts
+++ src/panel/types/Variable.ts
@@ -1,11 +1,8 @@
 import { type Handle } from './Handle';
 import { type BasciNodeData } from './BasicNode';
-import type Node from './BasicNode';
 
-interface VariableNodeData extends BasciNodeData {
+export interface VariableNodeData extends BasciNodeData {
   value: { title: string; handle: Handle };
   isConstant?: boolean;
   tooltip?: string;
 }
-
-export type VariableNode = Node<VariableNodeData>;
--- src/panel/types/index.ts
+++ src/panel/types/index.ts
@@ -2,6 +2,6 @@ export { DataType, isDataTypeMatch } from './DataType';
 export type { default as Node } from './BasicNode';
 export { type Handle } from './Handle';
 export { type FunctionNode, type FunctionNodeData } from './FunctionNode';
-export { type VariableNode } from './VariableNode';
+export { type VariableNodeData } from './Variable';
 export { type Edge } from './Edge';
 export { type Graph } from './Graph';
