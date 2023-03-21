47587c6eee786908dd93954086b7e301fcb3ced5
Fei Chen <boxchenfei@gmail.com>
2023-03-21 23:05:03 +0100

    Add function to check whether it is comment node

--- src/panel/types/Comment.ts
+++ src/panel/types/Comment.ts
@@ -3,3 +3,7 @@ import { type BasciNodeData } from './BasicNode';
 export interface Comment extends BasciNodeData {
   comment: string;
 }
+
+export function isCommentNode(nodeData: BasciNodeData): boolean {
+  return 'comment' in nodeData;
+}
