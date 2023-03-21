4e1d5077c72eb5cb0102b49cdcee2c245aa67de2
Fei Chen <boxchenfei@gmail.com>
2023-03-21 22:13:38 +0100

    Allow the comment node resizable (#31)

--- src/panel/components/nodes/CommentNode.tsx
+++ src/panel/components/nodes/CommentNode.tsx
@@ -1,10 +1,18 @@
 import React, { memo } from 'react';
 import './CommentNode.css';
 import { type Comment } from '../../types';
+import { NodeResizer } from '@reactflow/node-resizer';
+import '@reactflow/node-resizer/dist/style.css';
 
 function CommentNode({ data }: { data: Comment }): JSX.Element {
   return (
     <div title={data.tooltip}>
+      <NodeResizer
+        color="#ffffff00"
+        handleStyle={{ border: 'none' }}
+        minWidth={20}
+        minHeight={10}
+      />
       <div className="comment-node__header">
         This is a <strong>comment node</strong>
       </div>
