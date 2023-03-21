4eb0697602d2c5f2d3c5c089b181382704aa9782
Fei Chen <boxchenfei@gmail.com>
2023-03-21 11:02:27 +0100

    Create a setter node component (#55)

--- src/panel/components/nodes/index.ts
+++ src/panel/components/nodes/index.ts
@@ -1,11 +1,13 @@
 import CustomeNode from './CustomNode';
 import FunctionNode from './FunctionNode';
 import GetterNode from './GetterNode';
+import SetterNode from './SetterNode';
 
 const nodeTypes = {
   custom: CustomeNode,
   function: FunctionNode,
   getter: GetterNode,
+  setter: SetterNode,
 };
 
 export default nodeTypes;
