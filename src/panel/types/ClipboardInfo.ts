90839c642c16d39027ddaa90f5d2aabee1f2f49a
Fei Chen <boxchenfei@gmail.com>
2023-03-23 15:42:36 +0100

    Set the record type for nodes

--- src/panel/types/ClipboardInfo.ts
+++ src/panel/types/ClipboardInfo.ts
@@ -2,7 +2,7 @@ import type Node from './BasicNode';
 import { type Edge } from './Edge';
 export interface ClipboardInfo {
   isEmpty: boolean;
-  nodes: Node[];
+  nodes: Record<string, Node>;
   edges: Edge[];
   minX: number;
   minY: number;
