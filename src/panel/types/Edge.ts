3540377d9d1a8eb254a16b0372e704b64c3259d2
Fei Chen <boxchenfei@gmail.com>
2023-03-29 20:29:25 +0200

    Change data type from enum to object, adding options for the type (#101)

--- src/panel/types/Edge.ts
+++ src/panel/types/Edge.ts
@@ -1,9 +1,8 @@
 import { type Edge as RfEdge } from 'reactflow';
-import { type DataType } from './DataType';
 import { type BasciNodeData } from './BasicNode';
 
 export interface EdgeData extends BasciNodeData {
-  dataType?: DataType;
+  dataType?: string;
 }
 
 export type Edge = RfEdge<EdgeData>;
