b25be5f3441708247758b9ccb657635126d34ac3
Fei Chen <boxchenfei@gmail.com>
2023-03-29 15:21:07 +0200

    Add the default props for the elements

--- src/panel/widgets/WidgetFactory.tsx
+++ src/panel/widgets/WidgetFactory.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { NumberInput } from './Widgets';
 import { DataType } from '.././types';
+import { type WidgetProps } from './WidgetProps';
 
 export class WidgetFactory {
   private static instance: WidgetFactory;
+  private readonly defaultWidgetProps: WidgetProps = {
+    value: undefined,
+    onChange: (value: any): void => {
+      throw new Error('Function not implemented.');
+    },
+  };
+
   private readonly dataTypeToWidgetType: Record<string, string> = {
     [DataType.float]: 'NumberInput',
   };
 
   private readonly _availableWidgets: Record<string, JSX.Element> = {
-    NumberInput: (
-      <NumberInput
-        value={undefined}
-        onChange={function (value: any): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
-    ),
+    NumberInput: <NumberInput {...this.defaultWidgetProps} />,
   };
 
   private constructor() {}
