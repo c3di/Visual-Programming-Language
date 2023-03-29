c76bbd51933b9884268c707c54a616422f10c84c
Fei Chen <boxchenfei@gmail.com>
2023-03-29 13:40:27 +0200

    Fix `onChange` is not a function (#96)

--- src/panel/components/Handle.tsx
+++ src/panel/components/Handle.tsx
@@ -65,7 +65,7 @@ export default function Handle({
           (toHideWidgetWhenConnected && !isConnected))
           ? widgetFactory.createWidget('NumberInput', {
               value: handleData.defaultValue,
-              changeValue,
+              onChange: changeValue,
             })
           : null}
       </label>
