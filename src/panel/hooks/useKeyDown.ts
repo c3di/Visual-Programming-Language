0eda6ca42b330de0aed9e41f1a5a4a61ce4be579
Fei Chen <boxchenfei@gmail.com>
2023-03-30 11:21:40 +0200

    Format

--- src/panel/hooks/useKeyDown.ts
+++ src/panel/hooks/useKeyDown.ts
@@ -6,45 +6,53 @@ export default function useKeyDown(sceneState: SceneState): {
   onKeyDown: (e: React.KeyboardEvent<HTMLDivElement>) => void;
 } {
   const selectAllKeyPressed = useKeyPress('Control+a');
-  const cancelAllKeyPressed = useKeyPress('Escape');
-  const copyKeyPressed = useKeyPress('Control+c');
-  const pasteKeyPressed = useKeyPress('Control+v');
-  const deleteKeyPressed = useKeyPress('Delete');
-  const duplicateKeyPressed = useKeyPress('Control+d');
-  const cutKeyPressed = useKeyPress('Control+x');
-
   useEffect(() => {
-    if (selectAllKeyPressed) sceneState.selectAll(true);
+    if (selectAllKeyPressed) {
+      console.log('selectAllKeyPressed', selectAllKeyPressed);
+      sceneState.selectAll(true);
+    }
   }, [selectAllKeyPressed]);
+
+  const cancelAllKeyPressed = useKeyPress('Escape');
   useEffect(() => {
     console.log('cancelAllKeyPressed', cancelAllKeyPressed);
     if (!cancelAllKeyPressed) sceneState.selectAll(false);
   }, [cancelAllKeyPressed]);
+
+  const copyKeyPressed = useKeyPress('Control+c');
   useEffect(() => {
     if (copyKeyPressed) {
       sceneState.copySelectedNodeToClipboard();
     }
   }, [copyKeyPressed]);
+
+  const pasteKeyPressed = useKeyPress('Control+v');
   useEffect(() => {
     if (pasteKeyPressed) {
       sceneState.pasteFromClipboard();
     }
   }, [pasteKeyPressed]);
-  useEffect(() => {
-    if (deleteKeyPressed) {
-      sceneState.deleteSelectedElements();
-    }
-  }, [deleteKeyPressed]);
+
+  const duplicateKeyPressed = useKeyPress('Control+d');
   useEffect(() => {
     if (duplicateKeyPressed) {
       sceneState.duplicateSelectedNodes();
     }
   }, [duplicateKeyPressed]);
+
+  const cutKeyPressed = useKeyPress('Control+x');
   useEffect(() => {
     if (cutKeyPressed) {
       sceneState.cutSelectedNodesToClipboard();
     }
   }, [cutKeyPressed]);
+
+  const deleteKeyPressed = useKeyPress('Delete');
+  useEffect(() => {
+    if (deleteKeyPressed) {
+      sceneState.deleteSelectedElements();
+    }
+  }, [deleteKeyPressed]);
 
   const onKeyDown = useCallback((e: React.KeyboardEvent<HTMLDivElement>) => {
     console.log('onKeyDown', e.key, e.ctrlKey, e.shiftKey, e.altKey);
