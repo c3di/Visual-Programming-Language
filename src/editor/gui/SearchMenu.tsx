bbfd33080918a079f16d477ba3c766282bc2920f
Fei Chen <boxchenfei@gmail.com>
2023-04-30 22:11:18 +0200

    Move searchedtreeview out of menu

--- src/editor/gui/SearchMenu.tsx
+++ src/editor/gui/SearchMenu.tsx
@@ -1,176 +1,11 @@
 import React, { memo, useCallback, useEffect, useState } from 'react';
-import { IconButton, Input, InputAdornment, Menu } from '@mui/material';
-import { TreeView, TreeItem } from '@mui/lab';
-import { Search, Clear, ExpandMore, ChevronRight } from '@mui/icons-material';
-import { type NodeConfig } from '../types';
+import { Menu } from '@mui/material';
 import { type Command } from '../hooks';
-
-let itemId = 0;
-
-interface TreeItemData {
-  id: string;
-  name: string;
-  configType?: string;
-  children?: readonly TreeItemData[];
-  tooltip?: string;
-}
-
-const nodeConfigsToTreeData = (
-  nodeConfigs: Record<string, NodeConfig>
-): TreeItemData[] => {
-  const data: TreeItemData[] = [];
-  for (const name in nodeConfigs) {
-    const config = nodeConfigs[name];
-    if (config.notShowInMenu) continue;
-    const itemData = nodeConfigToTreeItemData(name, config);
-    if (itemData) data.push(itemData);
-  }
-  return data;
-};
-
-const nodeConfigToTreeItemData = (
-  name: string,
-  nodeConfig: any
-): TreeItemData | undefined => {
-  if (nodeConfig.notShowInMenu) return;
-  const children = [];
-  for (const name in nodeConfig.__isPackage__ ? nodeConfig.nodes : {}) {
-    const config = nodeConfig.nodes[name];
-    if (config.notShowInMenu) continue;
-    const itemData = nodeConfigToTreeItemData(name, config);
-    if (itemData) children.push(itemData);
-  }
-
-  return {
-    id: String(itemId++),
-    name,
-    configType: nodeConfig.type,
-    children: children.length > 0 ? children : undefined,
-    tooltip: nodeConfig.tooltip,
-  };
-};
-
-function SearchInput({
-  onChange,
-}: {
-  onChange: (value: string) => void;
-}): JSX.Element {
-  const [hasInput, setHasInput] = useState(false);
-  const inputRef = React.useRef<HTMLInputElement>(null);
-
-  return (
-    <Input
-      inputRef={inputRef}
-      placeholder="Search"
-      id="input-with-icon-adornment"
-      startAdornment={
-        <InputAdornment position="start">
-          {hasInput ? (
-            <IconButton
-              sx={{ padding: 0 }}
-              onClick={() => {
-                if (inputRef.current) inputRef.current.value = '';
-                setHasInput(false);
-                onChange('');
-              }}
-            >
-              <Clear />
-            </IconButton>
-          ) : (
-            <Search />
-          )}
-        </InputAdornment>
-      }
-      onChange={(e) => {
-        setHasInput(e.target.value.length > 0);
-        onChange(e.target.value);
-      }}
-    />
-  );
-}
-
-function ControlledTreeView({
-  treeData,
-  toExpand,
-  onItemClick,
-  onClose,
-  commands,
-}: {
-  treeData: TreeItemData[];
-  toExpand: boolean;
-  onItemClick: (configType: string | undefined) => void;
-  onClose: () => void;
-  commands?: Command[];
-}): JSX.Element {
-  const [expanded, setExpanded] = useState<string[]>([]);
-  const handleToggle = (e: React.SyntheticEvent, nodeIds: string[]): void => {
-    setExpanded(nodeIds);
-  };
-  const treeItemIds = useCallback((item: TreeItemData): string[] => {
-    const ids: string[] = [item.id];
-    for (const child of item.children ?? []) {
-      ids.push(...treeItemIds(child));
-    }
-    return ids;
-  }, []);
-  const treeDataIds = treeData.map((item) => treeItemIds(item)).flat();
-
-  useEffect(() => {
-    if (toExpand) setExpanded(treeDataIds);
-    else setExpanded([]);
-  }, [toExpand]);
-
-  const renderTreeItem = useCallback(
-    (item: TreeItemData): JSX.Element => (
-      <TreeItem
-        key={item.id}
-        nodeId={item.id}
-        label={item.name}
-        onClick={() => {
-          if (!Array.isArray(item.children)) onItemClick(item.configType);
-        }}
-        title={item.tooltip}
-      >
-        {Array.isArray(item.children)
-          ? item.children.map((node) => renderTreeItem(node))
-          : null}
-      </TreeItem>
-    ),
-    []
-  );
-
-  return (
-    <TreeView
-      aria-label="nodes types"
-      defaultCollapseIcon={<ExpandMore />}
-      defaultExpandIcon={<ChevronRight />}
-      expanded={expanded}
-      onNodeToggle={handleToggle}
-      sx={{
-        height: 250,
-        flexGrow: 1,
-        width: 230,
-        overflowY: 'auto',
-        overflowX: 'hidden',
-      }}
-    >
-      {treeData.map((root) => renderTreeItem(root))}
-      {commands?.map((command) => (
-        <TreeItem
-          key={command.name}
-          nodeId={command.name}
-          label={command.name}
-          onClick={() => {
-            command.action();
-            onClose();
-          }}
-          title={command.tooltip}
-        />
-      ))}
-    </TreeView>
-  );
-}
-
+import {
+  SearchedTreeView,
+  nodeConfigsToTreeData,
+  type TreeItemData,
+} from './SearchedTreeView';
 const SearchMenu = memo(function SearchMenu({
   open,
   onClose,
@@ -186,52 +21,6 @@ const SearchMenu = memo(function SearchMenu({
   addNode: (configType: string) => void;
   moreCommands?: Command[];
 }): JSX.Element {
-  const [treeData] = useState<TreeItemData[]>(
-    nodeConfigsToTreeData(nodeConfigs)
-  );
-  const [filteredTreeData, setFilteredTreeData] =
-    useState<TreeItemData[]>(treeData);
-  const [toExapand, setToExapand] = useState<boolean>(false);
-
-  const filteredTreeItemData = (
-    item: TreeItemData,
-    searchKeyword: string
-  ): TreeItemData | null => {
-    if (
-      item.name.toLocaleLowerCase().includes(searchKeyword.toLocaleLowerCase())
-    )
-      return { ...item };
-    const children: TreeItemData[] = [];
-    for (const child of item.children ?? []) {
-      const fItem = filteredTreeItemData(child, searchKeyword);
-      if (fItem) children.push(fItem);
-    }
-    if (children?.length) return { ...item, children };
-    return null;
-  };
-
-  const search = useCallback((searchKeyword: string) => {
-    if (searchKeyword === '') {
-      setFilteredTreeData(treeData);
-      setToExapand(false);
-    } else {
-      const filteredTreeData: TreeItemData[] = [];
-      for (const item of treeData) {
-        const fItem = filteredTreeItemData(item, searchKeyword);
-        if (fItem) filteredTreeData.push(fItem);
-      }
-      setFilteredTreeData(filteredTreeData);
-      setToExapand(true);
-    }
-  }, []);
-
-  const onItemClick = useCallback((configType: string | undefined): void => {
-    if (configType) {
-      addNode(configType);
-      onClose();
-    }
-  }, []);
-
   const [commands, setCommand] = useState<Command[]>([
     {
       name: 'Add Comment...',
@@ -262,6 +51,38 @@ const SearchMenu = memo(function SearchMenu({
     setCommand([...commands, ...newCommands]);
   }, [moreCommands]);
 
+  const [treeData, setTreeData] = useState<TreeItemData[]>(
+    nodeConfigsToTreeData(nodeConfigs)
+  );
+
+  const onItemClick = useCallback((item: TreeItemData): void => {
+    if (!item) return;
+    if (Array.isArray(item.children)) return;
+    if (item.configType) {
+      addNode(item.configType);
+      onClose();
+    }
+  }, []);
+
+  const commandsToTreeData = useCallback(
+    (commands: Command[]): TreeItemData[] => {
+      return commands.map((command) => ({
+        id: command.name,
+        name: command.name,
+        tooltip: command.tooltip,
+        onClick: () => {
+          command.action();
+          onClose();
+        },
+      }));
+    },
+    []
+  );
+
+  useEffect(() => {
+    setTreeData((treeData) => [...treeData, ...commandsToTreeData(commands)]);
+  }, [commands]);
+
   return (
     <Menu
       transitionDuration={0}
@@ -273,14 +94,7 @@ const SearchMenu = memo(function SearchMenu({
       anchorReference="anchorPosition"
       anchorPosition={anchorPosition}
     >
-      <SearchInput onChange={search} />
-      <ControlledTreeView
-        treeData={filteredTreeData}
-        toExpand={toExapand}
-        onItemClick={onItemClick}
-        onClose={onClose}
-        commands={commands}
-      />
+      <SearchedTreeView treeData={treeData} onItemClick={onItemClick} />
     </Menu>
   );
 });
---
+++ src/editor/gui/SearchedTreeView.tsx
@@ -0,0 +1,212 @@
+import React, { memo, useCallback, useEffect, useState } from 'react';
+import { IconButton, Input, InputAdornment } from '@mui/material';
+import { TreeView, TreeItem } from '@mui/lab';
+import { Search, Clear, ExpandMore, ChevronRight } from '@mui/icons-material';
+import { type NodeConfig } from '../types';
+
+let itemId = 0;
+
+export interface TreeItemData {
+  id: string;
+  name: string;
+  configType?: string;
+  children?: readonly TreeItemData[];
+  tooltip?: string;
+  onClick?: (item: any) => void;
+}
+
+export const nodeConfigsToTreeData = (
+  nodeConfigs: Record<string, NodeConfig>
+): TreeItemData[] => {
+  const data: TreeItemData[] = [];
+  for (const name in nodeConfigs) {
+    const config = nodeConfigs[name];
+    if (config.notShowInMenu) continue;
+    const itemData = nodeConfigToTreeItemData(name, config);
+    if (itemData) data.push(itemData);
+  }
+  return data;
+};
+
+const nodeConfigToTreeItemData = (
+  name: string,
+  nodeConfig: any
+): TreeItemData | undefined => {
+  if (nodeConfig.notShowInMenu) return;
+  const children = [];
+  for (const name in nodeConfig.__isPackage__ ? nodeConfig.nodes : {}) {
+    const config = nodeConfig.nodes[name];
+    if (config.notShowInMenu) continue;
+    const itemData = nodeConfigToTreeItemData(name, config);
+    if (itemData) children.push(itemData);
+  }
+
+  return {
+    id: String(itemId++),
+    name,
+    configType: nodeConfig.type,
+    children: children.length > 0 ? children : undefined,
+    tooltip: nodeConfig.tooltip,
+  };
+};
+
+function SearchInput({
+  onChange,
+}: {
+  onChange: (value: string) => void;
+}): JSX.Element {
+  const [hasInput, setHasInput] = useState(false);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  return (
+    <Input
+      inputRef={inputRef}
+      placeholder="Search"
+      id="input-with-icon-adornment"
+      startAdornment={
+        <InputAdornment position="start">
+          {hasInput ? (
+            <IconButton
+              sx={{ padding: 0 }}
+              onClick={() => {
+                if (inputRef.current) inputRef.current.value = '';
+                setHasInput(false);
+                onChange('');
+              }}
+            >
+              <Clear />
+            </IconButton>
+          ) : (
+            <Search />
+          )}
+        </InputAdornment>
+      }
+      onChange={(e) => {
+        setHasInput(e.target.value.length > 0);
+        onChange(e.target.value);
+      }}
+    />
+  );
+}
+
+function ControlledTreeView({
+  toExpand,
+  treeData,
+  onItemClick,
+}: {
+  toExpand: boolean;
+  treeData: TreeItemData[];
+  onItemClick?: (item: TreeItemData) => void;
+}): JSX.Element {
+  const [expanded, setExpanded] = useState<string[]>([]);
+  const handleToggle = (e: React.SyntheticEvent, nodeIds: string[]): void => {
+    setExpanded(nodeIds);
+  };
+  const treeItemIds = useCallback((item: TreeItemData): string[] => {
+    const ids: string[] = [item.id];
+    for (const child of item.children ?? []) {
+      ids.push(...treeItemIds(child));
+    }
+    return ids;
+  }, []);
+  const treeDataIds = treeData.map((item) => treeItemIds(item)).flat();
+
+  useEffect(() => {
+    if (toExpand) setExpanded(treeDataIds);
+    else setExpanded([]);
+  }, [toExpand]);
+
+  const renderTreeItem = useCallback(
+    (item: TreeItemData): JSX.Element => (
+      <TreeItem
+        key={item.id}
+        nodeId={item.id}
+        label={item.name}
+        onClick={() => {
+          if (item.onClick) item.onClick(item);
+          else onItemClick?.(item);
+        }}
+        title={item.tooltip}
+      >
+        {Array.isArray(item.children)
+          ? item.children.map((node) => renderTreeItem(node))
+          : null}
+      </TreeItem>
+    ),
+    []
+  );
+
+  return (
+    <TreeView
+      aria-label="nodes types"
+      defaultCollapseIcon={<ExpandMore />}
+      defaultExpandIcon={<ChevronRight />}
+      expanded={expanded}
+      onNodeToggle={handleToggle}
+      sx={{
+        height: 250,
+        flexGrow: 1,
+        width: 230,
+        overflowY: 'auto',
+        overflowX: 'hidden',
+      }}
+    >
+      {treeData.map((root) => renderTreeItem(root))}
+    </TreeView>
+  );
+}
+
+export const SearchedTreeView = memo(function SearchedTreeView({
+  treeData,
+  onItemClick,
+}: {
+  treeData: TreeItemData[];
+  onItemClick: (item: TreeItemData) => void;
+}): JSX.Element {
+  const [filteredTreeData, setFilteredTreeData] =
+    useState<TreeItemData[]>(treeData);
+  const [toExapand, setToExapand] = useState<boolean>(false);
+
+  const filteredTreeItemData = (
+    item: TreeItemData,
+    searchKeyword: string
+  ): TreeItemData | null => {
+    if (
+      item.name.toLocaleLowerCase().includes(searchKeyword.toLocaleLowerCase())
+    )
+      return { ...item };
+    const children: TreeItemData[] = [];
+    for (const child of item.children ?? []) {
+      const fItem = filteredTreeItemData(child, searchKeyword);
+      if (fItem) children.push(fItem);
+    }
+    if (children?.length) return { ...item, children };
+    return null;
+  };
+
+  const search = useCallback((searchKeyword: string) => {
+    if (searchKeyword === '') {
+      setFilteredTreeData(treeData);
+      setToExapand(false);
+    } else {
+      const filteredTreeData: TreeItemData[] = [];
+      for (const item of treeData) {
+        const fItem = filteredTreeItemData(item, searchKeyword);
+        if (fItem) filteredTreeData.push(fItem);
+      }
+      setFilteredTreeData(filteredTreeData);
+      setToExapand(true);
+    }
+  }, []);
+
+  return (
+    <>
+      <SearchInput onChange={search} />
+      <ControlledTreeView
+        toExpand={toExapand}
+        treeData={filteredTreeData}
+        onItemClick={onItemClick}
+      />
+    </>
+  );
+});
