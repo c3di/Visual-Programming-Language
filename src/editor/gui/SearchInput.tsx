4aa95d7167d2e1cdf89c76f35bbde47dbca2f06d
Fei Chen <boxchenfei@gmail.com>
2023-05-13 14:17:22 +0200

    Adjust padding for input (#177)

--- src/editor/gui/SearchInput.tsx
+++ src/editor/gui/SearchInput.tsx
@@ -11,58 +11,60 @@ export default function SearchInput({
   const inputRef = React.useRef<HTMLInputElement>(null);
 
   return (
-    <div style={{ marginLeft: '4px', marginRight: '4px', marginBottom: '4px' }}>
-      <OutlinedInput
-        sx={{
-          width: '100%',
-          height: '30px',
-          fontSize: '16px',
-          borderRadius: '0',
-          '&.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
-            borderColor: 'none',
+    <OutlinedInput
+      sx={{
+        width: '100%',
+        height: '30px',
+        fontSize: '16px',
+        borderRadius: '0',
+        padding: '0px',
+        '&.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
+          borderColor: 'none',
+        },
+        '&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':
+          {
+            border: '1px solid rgba(25, 118, 210, 1)',
+            boxShadow: 'inset 0 0 0 1.5px rgba(25, 118, 210, 0.2)',
           },
-          '&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':
-            {
-              border: '1px solid rgba(25, 118, 210, 1)',
-              boxShadow: 'inset 0 0 0 1.5px rgba(25, 118, 210, 0.2)',
-            },
-        }}
-        inputRef={inputRef}
-        placeholder="SEARCH"
-        id="input-with-icon-adornment"
-        endAdornment={
-          <InputAdornment position="start">
-            {hasInput ? (
-              <IconButton
-                sx={{ padding: 0 }}
-                onClick={() => {
-                  if (inputRef.current) inputRef.current.value = '';
-                  setHasInput(false);
-                  onChange('');
-                }}
-              >
-                <Clear
-                  sx={{
-                    width: '20px',
-                    marginRight: '-6px',
-                  }}
-                />
-              </IconButton>
-            ) : (
-              <Search
+        '& .MuiOutlinedInput-input': {
+          padding: '0px 0px 0px 6px',
+        },
+      }}
+      inputRef={inputRef}
+      placeholder="SEARCH"
+      id="input-with-icon-adornment"
+      endAdornment={
+        <InputAdornment position="start">
+          {hasInput ? (
+            <IconButton
+              sx={{ padding: 0 }}
+              onClick={() => {
+                if (inputRef.current) inputRef.current.value = '';
+                setHasInput(false);
+                onChange('');
+              }}
+            >
+              <Clear
                 sx={{
                   width: '20px',
                   marginRight: '-6px',
                 }}
               />
-            )}
-          </InputAdornment>
-        }
-        onChange={(e) => {
-          setHasInput(e.target.value.length > 0);
-          onChange(e.target.value);
-        }}
-      />
-    </div>
+            </IconButton>
+          ) : (
+            <Search
+              sx={{
+                width: '20px',
+                marginRight: '-6px',
+              }}
+            />
+          )}
+        </InputAdornment>
+      }
+      onChange={(e) => {
+        setHasInput(e.target.value.length > 0);
+        onChange(e.target.value);
+      }}
+    />
   );
 }
