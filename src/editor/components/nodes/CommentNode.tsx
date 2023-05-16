14d0b328b204e698dbf74339b94bae5921a4e0ed
Fei Chen <boxchenfei@gmail.com>
2023-05-16 15:41:54 +0200

    Adjust the comment size when resizing

--- src/editor/components/nodes/CommentNode.tsx
+++ src/editor/components/nodes/CommentNode.tsx
@@ -1,23 +1,38 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import './CommentNode.css';
 import { type Comment } from '../../types';
-import { NodeResizer } from '@reactflow/node-resizer';
+import {
+  NodeResizer,
+  type ResizeDragEvent,
+  type ResizeParamsWithDirection,
+} from '@reactflow/node-resizer';
 import '@reactflow/node-resizer/dist/style.css';
 import { InPlaceTextArea } from '../../widgets';
 
 function CommentNode({ data }: { data: Comment }): JSX.Element {
+  const [commentWidth, setCommentWidth] = useState<number>(data.width ?? 250);
+  const [commentHeight, setCommentHeight] = useState<number>(
+    data.height ?? 150
+  );
   return (
     <div
       title={data.tooltip}
-      style={{ width: data.width ?? 250, height: data.height ?? 150 }}
+      style={{ width: commentWidth, height: commentHeight }}
     >
       <NodeResizer
         color="#ffffff00"
         handleStyle={{ border: 'none' }}
-        minWidth={20}
-        minHeight={10}
+        minWidth={200}
+        minHeight={100}
+        onResize={(
+          event: ResizeDragEvent,
+          params: ResizeParamsWithDirection
+        ) => {
+          setCommentWidth(params.width);
+          setCommentHeight(params.height);
+        }}
       />
-      <div className="node__header">
+      <div className="node__header" style={{ paddingBottom: '0px' }}>
         <InPlaceTextArea text={data.comment} />
       </div>
       <div className="node__body"></div>
