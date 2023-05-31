2b61a6d576e426ce418698835d26bdb4e9fda938
Ziwei He <ziweihe1031@gmail.com>
2023-05-31 19:00:10 +0200

    clean styled TreeItem, move to sx property

--- src/editor/gui/elements/StyledTreeItem.tsx
+++ src/editor/gui/elements/StyledTreeItem.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Box, styled } from '@mui/material';
+import { Box } from '@mui/material';
 import { Typography, type SvgIconProps } from '@mui/material';
 import { TreeItem, type TreeItemProps } from '@mui/lab';
-import './StyledTreeItem.css';
 
 // from https://mui.com/material-ui/react-tree-view/#GmailTreeView.tsx
 declare module 'react' {
   interface CSSProperties {
-    '--tree-view-color'?: string;
-    '--tree-view-bg-color'?: string;
+    '--vp-treeview-font-color'?: string;
+    '--vp-treeview-item-hover-bg-color'?: string;
   }
 }
 
@@ -21,8 +20,6 @@ type StyledTreeItemProps = TreeItemProps & {
   iconColor?: 'success' | 'error' | 'warning' | undefined;
   onItemDelete?: (id: string) => void;
 };
-
-const StyledTreeItemRoot = styled(TreeItem)(({ theme }) => ({}));
 
 export default function StyledTreeItem(
   props: StyledTreeItemProps
@@ -38,7 +35,26 @@ export default function StyledTreeItem(
     ...other
   } = props;
   return (
-    <StyledTreeItemRoot
+    <TreeItem
+      style={{
+        '--vp-treeview-font-color': '#222',
+        '--vp-treeview-item-hover-bg-color': '#f5f5f6f0',
+      }}
+      sx={{
+        '& .MuiTreeItem-content': {
+          paddingRight: 0,
+          paddingLeft: 0,
+        },
+        '& .MuiTreeItem-content .MuiTreeItem-label': {
+          paddingTop: '0.1rem',
+          fontSize: '1rem',
+        },
+        '& .MuiTreeItem-content.Mui-selected, & .MuiTreeItem-content.Mui-selected.Mui-focused':
+          {
+            backgroundColor: 'var(--vp-treeview-item-hover-bg-color)',
+            color: 'var(--vp-treeview-font-color)',
+          },
+      }}
       icon={
         LabelIcon &&
         (iconColor ? (
@@ -56,7 +72,12 @@ export default function StyledTreeItem(
         >
           <Typography
             variant="body1"
-            sx={{ fontWeight: 'inherit', flexGrow: 0.8 }}
+            sx={{
+              fontWeight: 'inherit',
+              flexGrow: 0.8,
+              fontSize: '1rem',
+              color: 'var(--vp-treeview-font-color)',
+            }}
           >
             {labelText}
           </Typography>
@@ -67,10 +88,6 @@ export default function StyledTreeItem(
           )}
         </Box>
       }
-      style={{
-        '--tree-view-color': color,
-        '--tree-view-bg-color': bgColor,
-      }}
       {...other}
     />
   );
