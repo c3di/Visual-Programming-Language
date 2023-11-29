ecb82c94cab61e8350ee6edee931e6d28f084de9
Luojinxuan Wang <luwa00002@stud.uni-saarland.de>
2023-11-29 20:01:52 +0100

    kornia.equalization

---
+++ tests/codeGenerator/korniaEqualizationExecution.test.ts
@@ -0,0 +1,252 @@
+import { nodeExecCheck } from '../execution';
+import { loadNode } from '../loader';
+
+interface testNodeData {
+  jsonPath: string;
+  nodeName: string;
+  prepareInput: string;
+  inputs: string[];
+  returnVar: string;
+  execTest: (inputs: any[], returnVar: any) => string;
+  getExpectedCode: (
+    inputs: any[],
+    prepareInput: string,
+    returnVar: any,
+    execTest: (arg0: any, arg1: any) => any
+  ) => string;
+}
+
+describe('Code Execution of node kornia equalization', () => {
+  const testData: testNodeData[] = [
+    {
+      jsonPath: 'src/NodeTypeExtension/kornia/equalization.json',
+      nodeName: 'Equalize',
+      prepareInput: `import torch
+input_tensor1 = {
+  'dataType': 'torch.tensor',
+  'value': torch.rand(1, 1, 5, 5, device = 'cpu'),
+  'metadata': {
+    'colorChannel': 'grayscale',
+    'channelOrder': 'channelFirst',
+    'isMiniBatched': True,
+    'intensityRange': '0-1',
+    'device': 'cpu'
+  }
+}`,
+      inputs: ['', 'input_tensor1'],
+      returnVar: 'image',
+
+      execTest: (inputs: any[], returnVar: any) => `import torch
+from torch import Tensor
+expected = K.enhance.equalize(input_tensor1['value'])
+print(torch.equal(expected[0], ${returnVar}['value'][0]) and torch.equal(expected[1], ${returnVar}['value'][1]));`,
+
+      getExpectedCode: (
+        inputs: any[],
+        prepareInput: string,
+        returnVar: any,
+        execTest: (arg0: any, arg1: any) => any
+      ) => `import kornia as K
+${prepareInput}
+${returnVar} = K.enhance.equalize(input_tensor1['value'])
+${returnVar} = {
+  'value': ${returnVar},
+  'dataType': 'torch.tensor',
+  'metadata': {
+    'colorChannel': 'colorChannel': 'rgb' if ${
+      inputs[1]
+    }['metadata']['colorChannel'] == 'rgb' else 'grayscale',
+    'channelOrder': 'channelFirst',
+    'isMiniBatched': True if ${
+      inputs[1]
+    }['metadata']['isMiniBatched'] == True else False,
+    'intensityRange': '0-1',
+    'device': 'cpu' if ${inputs[1]}['value'].get_device() == -1 else 'gpu'
+  }
+}
+${execTest(inputs, returnVar)}`,
+    },
+    {
+      jsonPath: 'src/NodeTypeExtension/kornia/equalization.json',
+      nodeName: 'Equalize',
+      prepareInput: `import torch
+input_tensor1 = {
+  'dataType': 'torch.tensor',
+  'value': torch.rand(1, 3, 5, 5, device = 'cpu'),
+  'metadata': {
+    'colorChannel': 'rgb',
+    'channelOrder': 'channelFirst',
+    'isMiniBatched': True,
+    'intensityRange': '0-1',
+    'device': 'cpu'
+  }
+}`,
+      inputs: ['', 'input_tensor1'],
+      returnVar: 'image',
+
+      execTest: (inputs: any[], returnVar: any) => `import torch
+from torch import Tensor
+expected = K.enhance.equalize(input_tensor1['value'])
+print(torch.equal(expected[0], ${returnVar}['value'][0]) and torch.equal(expected[1], ${returnVar}['value'][1]));`,
+
+      getExpectedCode: (
+        inputs: any[],
+        prepareInput: string,
+        returnVar: any,
+        execTest: (arg0: any, arg1: any) => any
+      ) => `import kornia as K
+${prepareInput}
+${returnVar} = K.enhance.equalize(input_tensor1['value'])
+${returnVar} = {
+  'value': ${returnVar},
+  'dataType': 'torch.tensor',
+  'metadata': {
+    'colorChannel': 'colorChannel': 'rgb' if ${
+      inputs[1]
+    }['metadata']['colorChannel'] == 'rgb' else 'grayscale',
+    'channelOrder': 'channelFirst',
+    'isMiniBatched': True if ${
+      inputs[1]
+    }['metadata']['isMiniBatched'] == True else False,
+    'intensityRange': '0-1',
+    'device': 'cpu' if ${inputs[1]}['value'].get_device() == -1 else 'gpu'
+  }
+}
+${execTest(inputs, returnVar)}`,
+    },
+    {
+      jsonPath: 'src/NodeTypeExtension/kornia/equalization.json',
+      nodeName: 'Equalize_Clahe',
+      prepareInput: `import torch
+input_tensor1 = {
+  'dataType': 'torch.tensor',
+  'value': torch.rand(1, 1, 5, 5, device = 'cpu'),
+  'metadata': {
+    'colorChannel': 'grayscale',
+    'channelOrder': 'channelFirst',
+    'isMiniBatched': True,
+    'intensityRange': '0-1',
+    'device': 'cpu'
+  }
+}`,
+      inputs: ['', 'input_tensor1', '40.0', '(8, 8)', 'False'],
+      returnVar: 'image',
+
+      execTest: (inputs: any[], returnVar: any) => `import torch
+from torch import Tensor
+expected = K.enhance.equalize_clahe(input_tensor1['value'], ${inputs
+        .slice(2)
+        .join(', ')})
+print(torch.equal(expected, ${returnVar}['value']));`,
+
+      getExpectedCode: (
+        inputs: any[],
+        prepareInput: string,
+        returnVar: any,
+        execTest: (arg0: any, arg1: any) => any
+      ) => `import kornia as K
+${prepareInput}
+${returnVar} = K.enhance.equalize_clahe(input_tensor1['value'], ${inputs
+        .slice(2)
+        .join(', ')})
+${returnVar} = {
+  'value': ${returnVar},
+  'dataType': 'torch.tensor',
+  'metadata': {
+    'colorChannel': 'colorChannel': 'rgb' if ${
+      inputs[1]
+    }['metadata']['colorChannel'] == 'rgb' else 'grayscale',
+    'channelOrder': 'channelFirst',
+    'isMiniBatched': True if ${
+      inputs[1]
+    }['metadata']['isMiniBatched'] == True else False,
+    'intensityRange': '0-1',
+    'device': 'cpu' if ${inputs[1]}['value'].get_device() == -1 else 'gpu'
+  }
+}
+${execTest(inputs, returnVar)}`,
+    },
+    {
+      jsonPath: 'src/NodeTypeExtension/kornia/equalization.json',
+      nodeName: 'Equalize_Clahe',
+      prepareInput: `import torch
+input_tensor1 = {
+  'dataType': 'torch.tensor',
+  'value': torch.rand(1, 3, 5, 5, device = 'cpu'),
+  'metadata': {
+    'colorChannel': 'rgb',
+    'channelOrder': 'channelFirst',
+    'isMiniBatched': True,
+    'intensityRange': '0-1',
+    'device': 'cpu'
+  }
+}`,
+      inputs: ['', 'input_tensor1', '20.0', '(3, 3)', 'True'],
+      returnVar: 'image',
+
+      execTest: (inputs: any[], returnVar: any) => `import torch
+from torch import Tensor
+expected = K.enhance.equalize_clahe(input_tensor1['value'], ${inputs
+        .slice(2)
+        .join(', ')})
+print(torch.equal(expected, ${returnVar}['value']));`,
+
+      getExpectedCode: (
+        inputs: any[],
+        prepareInput: string,
+        returnVar: any,
+        execTest: (arg0: any, arg1: any) => any
+      ) => `import kornia as K
+${prepareInput}
+${returnVar} = K.enhance.equalize_clahe(input_tensor1['value'], ${inputs
+        .slice(2)
+        .join(', ')})
+${returnVar} = {
+  'value': ${returnVar},
+  'dataType': 'torch.tensor',
+  'metadata': {
+    'colorChannel': 'colorChannel': 'rgb' if ${
+      inputs[1]
+    }['metadata']['colorChannel'] == 'rgb' else 'grayscale',
+    'channelOrder': 'channelFirst',
+    'isMiniBatched': True if ${
+      inputs[1]
+    }['metadata']['isMiniBatched'] == True else False,
+    'intensityRange': '0-1',
+    'device': 'cpu' if ${inputs[1]}['value'].get_device() == -1 else 'gpu'
+  }
+}
+${execTest(inputs, returnVar)}`,
+    },
+  ];
+
+  test.each(testData)(
+    'generate the code from the node $nodeName in $jsonPath',
+    async ({
+      jsonPath,
+      nodeName,
+      prepareInput: prepareInput,
+      inputs,
+      returnVar,
+      execTest,
+      getExpectedCode: getExpectedOutput,
+    }) => {
+      const node = loadNode(jsonPath, nodeName);
+      const executeTest = execTest(inputs, returnVar);
+      const expectedOutput = getExpectedOutput(
+        inputs,
+        prepareInput,
+        returnVar,
+        execTest
+      );
+      await nodeExecCheck(
+        node,
+        inputs,
+        [executeTest, returnVar],
+        expectedOutput,
+        prepareInput
+      );
+    },
+    100000
+  );
+});
