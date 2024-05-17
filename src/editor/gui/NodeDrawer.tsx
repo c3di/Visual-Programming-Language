ba575f74e34ad61ccc7a56eb05962415f4cdc321
Luojinxuan Wang <luwa02@dfki.de>
2024-05-17 17:34:56 +0200

    integrate chonky into drawer

--- src/editor/gui/NodeDrawer.tsx
+++ src/editor/gui/NodeDrawer.tsx
@@ -1,9 +1,45 @@
-import { Box, Input, InputGroup, InputLeftElement, List, ListItem, VStack, Text, Icon, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
+import {
+    Box, Input, InputGroup, InputLeftElement, Text, Icon, Tabs, TabList, TabPanels, Tab, TabPanel,
+} from '@chakra-ui/react';
 import { Search2Icon } from '@chakra-ui/icons';
+import { ChonkyActions, FileBrowser, FileNavbar, FileData, FileList, FileHelper, ChonkyFileActionData, ChonkyIconName, FileViewConfig, FileViewMode } from 'chonky';
+import { ChonkyIconFA } from 'chonky-icon-fontawesome';
 import { nodeConfigRegistry } from '../extension';
-import { NodeConfig } from '../types';
+import { NodeConfig, NodePackage } from '../types';
 
-function NodeDrawer({
+const nodeConfigsToFileData = (
+    nodeConfigs: Record<string, NodeConfig | NodePackage>,
+    parentId: string = ''
+): FileData[] => {
+    return Object.entries(nodeConfigs).map(([name, config]) => {
+        const typedConfig = config as NodeConfig | NodePackage;
+        return {
+            id: parentId ? `${parentId}.${name}` : name,
+            name: 'title' in typedConfig ? typedConfig.title || name : name,
+            isDir: 'isPackage' in typedConfig ? true : false,
+            children: 'nodes' in typedConfig ? nodeConfigsToFileData(typedConfig.nodes, parentId ? `${parentId}.${name}` : name) : undefined,
+        };
+    });
+};
+
+interface CustomFileEntryProps {
+    file: FileData;
+}
+
+const CustomFileEntry: React.FC<CustomFileEntryProps> = ({ file }) => (
+    <Box
+        display="flex"
+        alignItems="center"
+        px={2}
+        py={1}
+        _hover={{ backgroundColor: 'gray.100' }}
+    >
+        <Text>{file.name}</Text>
+    </Box>
+);
+
+const NodeDrawer = ({
     handleNodeDragStart,
 }: {
     handleNodeDragStart: (
@@ -11,14 +47,89 @@ function NodeDrawer({
         nodeType: string,
         nodeData: NodeConfig
     ) => void;
-}) {
+}) => {
     const allNodeConfigs = nodeConfigRegistry.getAllNodeConfigs();
     const filteredNodeConfigs = Object.entries(allNodeConfigs).filter(
-        ([, config]) => Object.keys(config.nodes ?? {}).length > 0
+        ([, config]) => 'nodes' in config && Object.keys(config.nodes ?? {}).length > 0
     );
 
-    console.log('All Node Configs:', filteredNodeConfigs);
+    const [currentFolderId, setCurrentFolderId] = useState<string | null>(null);
+    const [breadcrumbFiles, setBreadcrumbFiles] = useState<FileData[]>([]);
+    const [currentFiles, setCurrentFiles] = useState<FileData[]>([]);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [currentTab, setCurrentTab] = useState<string | null>(null);
 
+    const loadFiles = (folderId: string) => {
+        const findNodeConfig = (configs: Record<string, NodeConfig | NodePackage>, id: string): NodeConfig | NodePackage | undefined => {
+            const path = id.split('.');
+            let node = configs[path[0]];
+            for (let i = 1; i < path.length; i++) {
+                if (node && 'nodes' in node) {
+                    node = node.nodes[path[i]] as NodePackage | NodeConfig;
+                } else {
+                    return undefined;
+                }
+            }
+            return node;
+        };
+
+        const nodeConfig = findNodeConfig(allNodeConfigs, folderId);
+        let files: FileData[] = [];
+        if (nodeConfig && 'nodes' in nodeConfig) {
+            files = nodeConfigsToFileData(nodeConfig.nodes, folderId);
+        }
+        setCurrentFiles(files);
+    };
+
+    const handleFileAction = (data: ChonkyFileActionData) => {
+        if (data.id === ChonkyActions.OpenFiles.id) {
+            const { targetFile } = data.payload;
+            if (targetFile && FileHelper.isDirectory(targetFile)) {
+                setCurrentFolderId(targetFile.id);
+                loadFiles(targetFile.id);
+                setBreadcrumbFiles((prev) => [...prev, targetFile]);
+            }
+        } else if (data.id === ChonkyActions.MouseClickFile.id) {
+            const file = data.payload.file as FileData;
+            if (file && !FileHelper.isDirectory(file)) {
+                const findNodeConfig = (configs: Record<string, NodeConfig | NodePackage>, id: string): NodeConfig | NodePackage | undefined => {
+                    const path = id.split('.');
+                    let node = configs[path[0]];
+                    for (let i = 1; i < path.length; i++) {
+                        if (node && 'nodes' in node) {
+                            node = node.nodes[path[i]] as NodePackage | NodeConfig;
+                        } else {
+                            return undefined;
+                        }
+                    }
+                    return node;
+                };
+
+                if (file) {
+                    const nodeConfig = findNodeConfig(allNodeConfigs, file['id']);
+                    if (nodeConfig && 'type' in nodeConfig) {
+                        const dragEvent = new DragEvent('dragstart', { bubbles: true, cancelable: true }) as unknown as React.DragEvent<HTMLLIElement>;
+                        handleNodeDragStart(dragEvent, nodeConfig.type, nodeConfig as NodeConfig);
+                    }
+                }
+            }
+        }
+    };
+
+    const handleBreadcrumbClick = (file: FileData, index: number) => {
+        const newBreadcrumbFiles = breadcrumbFiles.slice(0, index + 1);
+        setBreadcrumbFiles(newBreadcrumbFiles);
+        setCurrentFolderId(file.id);
+        loadFiles(file.id);
+    };
+
+    useEffect(() => {
+        if (currentTab !== null) {
+            setCurrentFolderId(currentTab);
+            loadFiles(currentTab);
+            setBreadcrumbFiles([{ id: currentTab, name: currentTab, isDir: true }]);
+        }
+    }, [currentTab]);
 
     return (
         <Box p={4} width="300px" bg="gray.50" borderRight="1px solid #ccc" height="100%" flexDirection="column">
@@ -27,71 +138,68 @@ function NodeDrawer({
                 <InputLeftElement pointerEvents="none" mb={4}>
                     <Icon as={Search2Icon} />
                 </InputLeftElement>
-                <Input placeholder="Search node" />
+                <Input
+                    placeholder="Search node"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
             </InputGroup>
-            <Tabs isFitted variant='soft-rounded' orientation="vertical" mt={8} maxHeight="100%"
+            <Tabs
+                isFitted
+                variant='soft-rounded'
+                orientation="vertical"
+                mt={8}
+                maxHeight="100%"
+                onChange={(index) => setCurrentTab(filteredNodeConfigs[index][0])}
                 sx={{
                     flex: 1,
                     display: 'flex',
                     flexDirection: 'row',
-                }}>
-                <TabList overflow="scroll" sx={{
-                    width: '80px',
-                    height: '100%',
-                    scrollbarWidth: 'none',
-                    '::-webkit-scrollbar': {
-                        display: 'none',
-                    }
                 }}
+            >
+                <TabList overflow="scroll"
+                    sx={{
+                        width: '80px',
+                        height: '100%',
+                        scrollbarWidth: 'none',
+                        '::-webkit-scrollbar': {
+                            display: 'none',
+                        }
+                    }}
                 >
-                    {
-                        filteredNodeConfigs.map(([category]) => (
-                            <Tab fontSize='xs' key={category} flexShrink={0}>{category}</Tab>
-                        ))
-                    }
+                    {filteredNodeConfigs.map(([category]) => (
+                        <Tab fontSize='xs' key={category} flexShrink={0}>{category}</Tab>
+                    ))}
                 </TabList>
                 <TabPanels
                     sx={{
                         flex: 1,
                         overflowY: 'auto',
+                        height: '100%',
                     }}>
-                    {filteredNodeConfigs.map(([category, config]) => {
-                        console.log('Category:', category);
-                        console.log('Config Nodes:', config.nodes);
-                        return (
-                            <TabPanel key={category} mt={2}>
-                                <VStack align="stretch">
-                                    <List spacing={2}>
-                                        {Object.values(config.nodes ?? {}).map((nodeConfig) => {
-                                            const typedNodeConfig = nodeConfig as NodeConfig;
-                                            console.log('Node Config:', typedNodeConfig); // Log each node configuration
-                                            return (
-                                                <ListItem
-                                                    key={typedNodeConfig.id}
-                                                    draggable
-                                                    onDragStart={(e) =>
-                                                        handleNodeDragStart(e, typedNodeConfig.type, typedNodeConfig)
-                                                    }
-                                                    opacity={0.5}
-                                                    cursor="pointer"
-                                                    p={2}
-                                                    borderRadius="md"
-                                                    bg="gray.100"
-                                                    _hover={{ bg: 'gray.200' }}
-                                                >
-                                                    {typedNodeConfig.title || typedNodeConfig.name}
-                                                </ListItem>
-                                            );
-                                        })}
-                                    </List>
-                                </VStack>
-                            </TabPanel>
-                        );
-                    })}
+                    {filteredNodeConfigs.map(([category]) => (
+                        <TabPanel key={category} mt={2} height="100%" display="flex" flexDirection="column">
+                            <Box flex="1" display="flex" flexDirection="column" height="100%">
+                                <FileBrowser
+                                    files={currentFiles}
+                                    folderChain={breadcrumbFiles}
+                                    onFileAction={handleFileAction}
+                                    defaultFileViewActionId={ChonkyActions.EnableListView.id}
+                                    disableDefaultFileActions
+                                    fileActions={[ChonkyActions.OpenFiles, ChonkyActions.MouseClickFile]}
+                                    iconComponent={ChonkyIconFA as any}
+                                >
+                                    <FileNavbar />
+                                    <FileList />
+                                    {CustomFileEntry as any}
+                                </FileBrowser>
+                            </Box>
+                        </TabPanel>
+                    ))}
                 </TabPanels>
             </Tabs>
-        </Box >
+        </Box>
     );
-}
+};
 
 export default NodeDrawer;
